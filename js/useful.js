7aa268713cc89ad9a4ae712757050d407716b6d0
Ben Doherty <ben@notionparallax.co.uk>
2014-07-29 17:39:59 +1000

    finishing off and failing comments, added over image text

--- js/useful.js
+++ js/useful.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+    /* * * Disqus Reset Function * * */
+    var resetD = function (newIdentifier, newUrl, newTitle, newLanguage) {
+        DISQUS.reset({
+            reload: true,
+            config: function () {
+                this.page.identifier = newIdentifier;
+                this.page.url = newUrl;
+                this.page.title = newTitle;
+                this.language = newLanguage;
+                console.log([this.page.identifier, this.page.url, this.page.title, this.language]);
+            }
+        });
+    };
+
     function activateText() {
+        var id = $(this).context.id;
         $("*").removeClass("highlighted");
-        $(".ben." + $(this).context.id).toggleClass("highlighted");
-        console.log("clicked " + $(this).context.id);
+        $( id).toggleClass("highlighted");
+
+        var itemTitle = $("."+id).html();
+        var itemBody  = $("."+id+" + dd").html();
+        $(".this-item").html("<h1>"+itemTitle+"</h1>"+"<p>"+itemBody+"<p>");
+        console.log("clicked " + id);
+        console.log([itemTitle,itemBody]);
     };
 
     function reRegisterEvents() {
@@ -10,13 +30,19 @@ $(document).ready(function() {
         $("path").click(activateText);
         $("rect").click(activateText);
     };
+    function hashbanger(value){
+        return "!"+value;
+    };
 
     $(".person").load("ben-doherty.html", reRegisterEvents);
 
     $(".person-picker").change(function() {
         console.log($(this).context.value);
         $(".person").load($(this).context.value, reRegisterEvents);
-        document.location.hash = $(this).context.value.split(".")[0];
-    });
 
+        var pageName = $(this).context.value.split(".")[0];
+        document.location.hash = hashbanger(pageName);
+        resetD(pageName, "http://notionparallax.github.io/kitGrids/#!"+pageName,pageName,'en');
+
+    });
 });
