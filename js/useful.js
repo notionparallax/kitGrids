105b5d5ac91fc03cffe43b72681b3999edf3c1d0
Ben Doherty <ben@notionparallax.co.uk>
2014-08-04 11:17:03 +1000

    started working on turning off the tabs in big screen

--- js/useful.js
+++ js/useful.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
     /* * * Disqus Reset Function * * */
-    var resetD = function (newIdentifier, newUrl, newTitle, newLanguage) {
+    var resetD = function(newIdentifier, newUrl, newTitle, newLanguage) {
         DISQUS.reset({
             reload: true,
-            config: function () {
+            config: function() {
                 this.page.identifier = newIdentifier;
                 this.page.url = newUrl;
                 this.page.title = newTitle;
@@ -18,12 +18,12 @@ $(document).ready(function() {
         var id = $(this).context.id;
         console.log("clicked " + id);
         $("*").removeClass("highlighted");
-        $( id).toggleClass("highlighted");
+        $(id).toggleClass("highlighted");
 
-        var itemTitle = $("."+id).html();
-        var itemBody  = $("."+id+" + dd").html();
-        $(".this-item").html("<h1>"+itemTitle+"</h1>"+"<p>"+itemBody+"<p>");
-        console.log([itemTitle,itemBody]);
+        var itemTitle = $("." + id).html();
+        var itemBody = $("." + id + " + dd").html();
+        $(".this-item").html("<h1>" + itemTitle + "</h1>" + "<p>" + itemBody + "<p>");
+        console.log([itemTitle, itemBody]);
     };
 
     function reRegisterEvents() {
@@ -32,31 +32,31 @@ $(document).ready(function() {
         console.log("reRegisterEvents run");
     };
 
-    function hashbanger(value){
-        return "!"+value;
+    function hashbanger(value) {
+        return "!" + value;
     };
 
-    function injectAperson(personName){
-        $("#items-receiver"    ).load(personName + ".html #items");
+    function injectAperson(personName) {
+        $("#items-receiver").load(personName + ".html #items");
         $("#interview-receiver").load(personName + ".html #interview");
-        $("#portrait-receiver" ).load(personName + ".html #portrait");
-        $("#svg-receiver"      ).load(personName + ".html #svg", reRegisterEvents);
+        $("#portrait-receiver").load(personName + ".html #portrait");
+        $("#svg-receiver").load(personName + ".html #svg", reRegisterEvents);
     }
 
     var personParam = document.URL.split('#!')[1];
-    if(personParam){
+    if (personParam) {
         console.log(personParam);
         injectAperson(personParam);
         // $(".person").load(personParam + ".html", reRegisterEvents);
-    }else{
+    } else {
         $(".person").load("front-page.html", reRegisterEvents);
     }
 
-    $(".show-menu").click(function(){
+    $(".show-menu").click(function() {
         $('nav').toggleClass('nav-visible');
     });
 
-    $("nav li a").click(function(event){
+    $("nav li a").click(function(event) {
         event.preventDefault();
 
         var selectedValue = $(this).data().person;
@@ -64,16 +64,51 @@ $(document).ready(function() {
 
         //set the selected option to selected
         $(this).prop('selected', false)
-               .filter('[value="'+selectedValue+'"]')
-               .prop('selected', true);
+            .filter('[value="' + selectedValue + '"]')
+            .prop('selected', true);
 
         //load the content into the middle of the page
         injectAperson(selectedValue);
         // reset the comments so that they point to the right page
         var pageName = selectedValue.split(".")[0];
         document.location.hash = hashbanger(pageName);
-        resetD(pageName, "http://notionparallax.github.io/kitGrids/#!"+pageName,pageName,'en');
+        resetD(pageName, "http://notionparallax.github.io/kitGrids/#!" + pageName, pageName, 'en');
 
         $('nav').toggleClass('nav-visible');
     });
+
+
+    enquire.register("screen and (max-width: 699px)", {
+
+        deferSetup: true,
+        setup: function() {
+            console.log("enquire setup called");
+            var tabsSwiper = new Swiper('.swiper-container', {
+                speed: 300,
+                onSlideChangeStart: function() {
+                    $(".tabs .active").removeClass('active')
+                    $(".tabs a").eq(tabsSwiper.activeIndex).addClass('active')
+                }
+            });
+        },
+        match: function() {
+            console.log("enquire match called");
+            $(".tabs a").on('touchstart mousedown', function(e) {
+                e.preventDefault()
+                $(".tabs .active").removeClass('active')
+                $(this).addClass('active')
+                tabsSwiper.swipeTo($(this).index())
+            });
+            $(".tabs a").click(function(e) {
+                e.preventDefault()
+            });
+        },
+        unmatch: function() {
+            console.log("enquire unmatch called");
+            tabsSwiper.destroy( true );
+        }
+
+    });
+
+
 });
